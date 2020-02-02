bf6c37c9eb48a146330583ee760c66475f973269
DariaDimchuk <daria.dimchuk@gmail.com>
2020-02-02 12:48:46 -0800

    validation

--- lab3/actors.js
+++ lab3/actors.js
@@ -1,12 +1,14 @@
 var id = 0;
 
 var actors = [
-    { Id: id++, Name: "Bella Donna", Description: "test descr", Img: "https://randomuser.me/api/portraits/med/women/1.jpg"},
-    { Id: id++, Name: "Ben Jer", Description: "test2", Img: "https://randomuser.me/api/portraits/med/men/1.jpg"}
+    { Id: id++, Name: "Bella Donna", Description: "Singer", Img: "https://randomuser.me/api/portraits/med/women/1.jpg"},
+    { Id: id++, Name: "Ben Jaskier", Description: "Award Winner", Img: "https://randomuser.me/api/portraits/med/men/1.jpg"}
 ];
 
 function toggleInsert(){
     var x = document.getElementById("addActor");
+    document.getElementById("add-actor-error").style.display = 'none';
+
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
@@ -21,22 +23,39 @@ function deleteArtist(){
     document.getElementById(this.value).remove(); //remove from html
 }
 
+
 function addActor() {
     var name = document.getElementById("name").value;
     var desc = document.getElementById("description").value;
     var avatar = document.getElementById("avatar").value;
 
+    var err = document.getElementById("add-actor-error");
+
+    if(name.length == 0 || desc.length == 0 || avatar.length == 0){
+        err.innerText = "Error: Name, Description and Image url are required fields.";
+        err.style.display = 'block';
+        return false;
+    }
+
+    if(name.length > 40 || desc.length > 40){
+        err.innerText = "Error: Name and Description must be < 40 characters.";
+        err.style.display = 'block';
+        return false;
+    }
+
+    //no errors detected
+    err.style.display = 'none';
+
     var actor = {
-      Id: id++,
-      Name: name,
-      Description: desc,
-      Img: avatar
+        Id: id++,
+        Name: name,
+        Description: desc,
+        Img: avatar
     };
 
-    actors.push(actor);
-
+    actors.push(actor); //updates in memory list
     toggleInsert(); //hides & resets form
-    displayActors();
+    displayActors(); //refreshes list
 }
 
 
@@ -46,7 +65,7 @@ function displayActors(){
 
     for(var i = 0; i < actors.length; i++){
         if(!document.getElementById(actors[i].Id)) { //only create element if it is not already in table
-            var div = document.createElement("div");
+            var div = document.createElement("div")
             div.setAttribute('id', actors[i].Id);
             div.className += " inner-flex artist-row";
 
