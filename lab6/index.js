e9cdf52779f9a24cba0f33a7fcd15d8f3f00be74
DariaDimchuk <daria.dimchuk@gmail.com>
2020-03-11 14:10:29 -0700

    added lab6 files (copy of lab5)

---
+++ lab6/index.js
@@ -0,0 +1,140 @@
+let express = require('express');
+let app = express();
+var http = require('http');
+let bodyparser = require('body-parser');
+
+const fs = require('fs');
+const path = require('path');
+
+const filePath = './actors.json';
+
+//app.listen(3000, () => console.log('Server ready')) //for local debug
+app.listen(process.env.PORT || 3000);
+
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "actors.html"));
+});
+
+
+/**
+ * Gets all actors from saved file.
+ */
+app.get('/getall', async function(req, res) {
+    try{
+        res.setHeader('Content-Type', 'application/json');
+
+        var actors = await getActorsFromFile(); //gets actors from json file
+        var json = JSON.stringify(actors);
+        res.setHeader('Content-Type', 'application/json');
+        res.end(json);
+    } catch(e){
+        res.end(e.message || e.toString());
+    }
+});
+
+
+/**
+ * Returns filtered list of actors that match the input search string.
+ */
+app.get('/search/:value', async function(req, res) {
+    try{
+        let name = req.params.value;
+
+        var actors = await getActorsFromFile();
+        var filteredActors = actors.filter(x => x.name.toLowerCase().includes(name));
+
+        var json = JSON.stringify(filteredActors);
+        res.setHeader('Content-Type', 'application/json');
+        res.end(json);
+    } catch(e){
+        res.end(e.message || e.toString());
+    }
+});
+
+
+/**
+ * Adds new actor.
+ */
+app.post('/add', async function(req, res) {
+    try{
+        let name = req.body.name;
+        let description = req.body.desc;
+        let avatarImg = req.body.url;
+
+        var id = generateId();
+
+        var actors = await getActorsFromFile();
+        actors.push({ id, name, description, avatarImg });
+
+        await saveActorsToFile(actors);
+        res.sendStatus(204);
+    } catch(e){
+        res.end(e.message || e.toString());
+    }
+});
+
+
+/**
+ * Deletes an actor.
+ */
+app.post('/delete', async function(req, res) {
+
+    try{
+        let id = req.body.idToDelete;
+
+        var actors = await getActorsFromFile();
+        actors = actors.filter(x => x.id != id);
+        await saveActorsToFile(actors);
+
+        res.sendStatus(204);
+    } catch(e){
+        res.end(e.message || e.toString());
+    }
+});
+
+
+/**
+ * Writes to actors.json file. Will replace entire file on save.
+ * @param actors
+ * @returns {Promise<void>}
+ */
+async function saveActorsToFile(actors){
+    await fs.writeFile(filePath, JSON.stringify(actors), function (err) {
+        if (err) return console.log(err);
+    });
+}
+
+
+/**
+ * Gets the actors array from json file. If doesn't exist, makes a new one.
+ * @returns {Promise<*>}
+ */
+async function getActorsFromFile(){
+    try {
+        await fs.promises.access(filePath); //check if exists
+
+        var raw = await fs.readFileSync(filePath);
+        var actors = JSON.parse(raw);
+        return actors;
+    } catch (error) {
+        await saveActorsToFile([]); //creates a new file & saves empty array
+        return [];
+    }
+}
+
+
+/**
+ * A unique Id made by using current date, and converting to seconds passed since 1970.
+ * @returns {number} - unique id
+ */
+function generateId(){
+    var d = new Date();
+    return Math.round(d / 1000);
+}
---
+++ lab6/public/actors.js
@@ -0,0 +1,225 @@
+class Actor {
+    id;
+    name;
+    description;
+    avatarImg;
+
+    constructor(id, name, descr, avatar) {
+        this.id = id;
+        this.name = name;
+        this.description = descr;
+        this.avatarImg = avatar;
+    }
+}
+
+var actors = [];
+
+
+/**
+ * Either loads all actors from backend "db", or searching for specifically named actors.
+ *
+ * @param search (optional) - if added, will search for actors whose name matches/contains the search
+ * @returns {Promise<void>}
+ */
+async function loadActors(search = "") {
+    if (search.length > 0) {
+        const response = await fetch("/search/" + search, {
+            method: "GET",
+            headers: {'Content-Type': 'application/json'}
+        });
+
+        actors = await response.json();
+    } else {
+        const response = await fetch('/getall');
+        actors = await response.json();
+    }
+}
+
+
+/**
+ * Load page by getting actors from backend, and displaying them
+ * @returns {Promise<void>}
+ */
+async function loadPage() {
+    await loadActors();
+    displayActors();
+}
+
+
+/**
+ * Search for specific actors by input name
+ * @returns {Promise<void>}
+ */
+async function search() {
+    var input = document.getElementById("search-input").value.toLowerCase();
+
+    await loadActors(input);
+    displayActors();
+}
+
+
+/**
+ * Delete chosen actor
+ * @returns {Promise<void>}
+ */
+async function deleteArtist() {
+    var id = document.getElementById(this.value).id;
+    var params = JSON.stringify({idToDelete: id});
+
+    await fetch("/delete", {
+        method: "POST",
+        body: params,
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    await loadPage();
+    showNoArtistsFoundMsg();
+}
+
+
+/**
+ * Add new actor
+ * @returns {Promise<boolean>}
+ */
+async function addActor() {
+    var name = document.getElementById("name").value;
+    var desc = document.getElementById("description").value;
+    var avatar = document.getElementById("avatar").value;
+
+    var err = document.getElementById("add-actor-error");
+
+    if (name.length == 0 || desc.length == 0 || avatar.length == 0) {
+        err.innerText = "Error: name, description and Image url are required fields.";
+        err.style.display = 'block';
+        return false;
+    }
+
+    if (name.length > 40 || desc.length > 40) {
+        err.innerText = "Error: name and description must be < 40 characters.";
+        err.style.display = 'block';
+        return false;
+    }
+
+    //no errors detected
+    err.style.display = 'none';
+
+    var params = JSON.stringify({name: name, desc: desc, url: avatar});
+
+    await fetch("/add", {
+        method: "POST",
+        body: params,
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    await loadActors();
+
+    toggleInsert(); //hides & resets form
+    displayActors(); //refreshes list
+}
+
+
+/**
+ * Fills the html and displays all actors.
+ */
+function displayActors() {
+    var actor_list_tbl = document.getElementById("artist-table");
+
+    while (actor_list_tbl.hasChildNodes()) {
+        actor_list_tbl.removeChild(actor_list_tbl.firstChild);
+    }
+
+    for (var i = 0; i < actors.length; i++) {
+        var actor_div = document.createElement("div");
+        actor_div.setAttribute('id', actors[i].id);
+        actor_div.className += " actor-flex-1 actor-element";
+
+        var avatarImg = document.createElement("img");
+        avatarImg.className += " actor-avatar";
+        avatarImg.src = actors[i].avatarImg;
+
+
+        var actor_name = document.createElement("span");
+        actor_name.className += " actor_name";
+        actor_name.textContent = actors[i].name;
+        actor_name.appendChild(document.createElement("br"));
+
+
+        var actor_details = document.createElement("div");
+        actor_details.className += " actor_details";
+
+        var actor_descr = document.createElement("span");
+        actor_descr.className += " desc_of_actor";
+        actor_descr.textContent = actors[i].description;
+
+        //must append in such an order
+        actor_details.appendChild(actor_name);
+        actor_details.appendChild(actor_descr);
+
+        var del = document.createElement("div");
+        del.className += "delete";
+        var delBtn = document.createElement("button");
+        delBtn.textContent += "Delete";
+        delBtn.value = actors[i].id;
+        delBtn.addEventListener("click", deleteArtist);
+        del.appendChild(delBtn);
+
+        actor_div.appendChild(avatarImg);
+        actor_div.appendChild(actor_details);
+        actor_div.appendChild(del);
+        actor_list_tbl.appendChild(actor_div);
+    }
+
+    showNoArtistsFoundMsg();
+}
+
+
+/**
+ * Toggles the "Add Actor" div to be hidden/visible
+ * */
+function toggleInsert() {
+    var x = document.getElementById("addActor");
+    document.getElementById("add-actor-error").style.display = 'none';
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        generateAvatarImg(); //TODO this is for easier actor creation
+    } else {
+        x.style.display = "none";
+        x.reset();
+    }
+}
+
+
+/**
+ * If there are no actors in the array, will show "Empty" message.
+ * Otherwise shows the actors.
+ */
+function showNoArtistsFoundMsg() {
+    var emptyTbl = document.getElementById("empty-table");
+    if (actors.length === 0) {
+        emptyTbl.style.display = "flex";
+    } else {
+        emptyTbl.style.display = "none";
+    }
+}
+
+
+/**
+ * Auto generates a random female or male avatar image.
+ * This is just for easier test purposes.
+ */
+function generateAvatarImg() {
+    var femUrl = "https://randomuser.me/api/portraits/med/women/";
+    var maleUrl = "https://randomuser.me/api/portraits/med/men/";
+
+    var extension = ".jpg";
+
+    var index = Math.floor(Math.random() * 99); //between 0 and 99
+
+    var chosenUrl = Math.random() < 0.5 ? femUrl : maleUrl;
+    var generatedAvatarUrl = chosenUrl + index + extension;
+
+    var avatarInput = document.getElementById("avatar");
+    avatarInput.value = generatedAvatarUrl;
+}
+
